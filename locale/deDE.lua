a692dbaa9ff05dd1ee482cef635f8f0c2aac0a06
wolftankk <wolftankk@007ea5aa-0d4a-491f-903c-63b4d4e33813>
2010-09-29 09:12:11 +0000

    Icetip: - add locale filename to the TOC.
    git-svn-id: svn://svn.curseforge.net/wow/icetip/mainline/trunk@25 007ea5aa-0d4a-491f-903c-63b4d4e33813

---
+++ locale/deDE.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Icetip", "deDE");
+if not L then return end
+
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat")@
