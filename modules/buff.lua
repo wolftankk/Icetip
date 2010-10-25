40220cffe1e73f35e2e2f0b64a062d4ed49e959c
wolftankk <wolftankk@007ea5aa-0d4a-491f-903c-63b4d4e33813>
2010-10-25 10:21:33 +0000

    Icetip: - add Buff module
    git-svn-id: svn://svn.curseforge.net/wow/icetip/mainline/trunk@59 007ea5aa-0d4a-491f-903c-63b4d4e33813

---
+++ modules/buff.lua
@@ -0,0 +1,18 @@
+local addonName, Icetip = ...;
+local mod = Icetip:NewModule("Buff");
+
+function mod:OnEnable()
+
+end
+
+function mod:OnDisable()
+
+end
+
+function mod:OnTooltipShow()
+
+end
+
+function mod:OnTooltipHide()
+
+end
