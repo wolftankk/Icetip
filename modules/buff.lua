296ffcb6dc8d81eaabb42cafcaee3d31d845ba35
wolftankk <wolftankk@007ea5aa-0d4a-491f-903c-63b4d4e33813>
2011-11-14 11:19:20 +0000

    Icetip: - Add test frame for Buff module
    git-svn-id: svn://svn.curseforge.net/wow/icetip/mainline/trunk@70 007ea5aa-0d4a-491f-903c-63b4d4e33813

--- modules/buff.lua
+++ modules/buff.lua
@@ -1,16 +1,51 @@
 local addonName, Icetip = ...;
 local mod = Icetip:NewModule("Buff");
+local db
+--update function
+local update
+
+local buffFrame, debuffFrame;
 
 function mod:OnEnable()
+	local db = self.db["auras"];
+	self.db = db;
 
+	--create buffFrame and debuffFrame if the mode enable
 end
 
 function mod:OnDisable()
 
 end
 
-function mod:OnTooltipShow()
+function mod:CreateAuraFrame()
+end
 
+function mod:OnTooltipShow()
+	if buffFrame then return end
+	buffFrame = CreateFrame("Frame", nil, GameTooltip);
+	--buff frame grow dir
+	buffFrame.growBy = "up";
+	buffFrame:SetBackdrop({
+		bgFile = [[Interface/Tooltips/UI-Tooltip-Background]],
+		edgetFile = [[Interface/Tooltips/UI-Tooltip-Border]],
+		tile = false,
+		tileSize = 8,
+		edgetSize = 16,
+		insets = {
+			left = 5,
+			right = 5,
+			top = 5,
+			bottom = 5
+		}
+	});
+	buffFrame:SetBackdropBorderColor(0, 0, 0, 0.6);
+	buffFrame:SetBackdropColor(0, 0, 0, 0.5);
+	
+
+	--position
+	buffFrame:SetPoint("BOTTOMLEFT", GameTooltip, "TOPLEFT", 2, 0);
+	buffFrame:SetPoint("BOTTOMRIGHT", GameTooltip, "TOPRIGHT", 2, 0);
+	buffFrame:SetHeight(100)
 end
 
 function mod:OnTooltipHide()
