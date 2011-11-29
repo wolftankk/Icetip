fcf5aadbab47b9d78a8b9aa79e6461d959a667c4
wolftankk <wolftankk@007ea5aa-0d4a-491f-903c-63b4d4e33813>
2011-11-29 04:58:22 +0000

    Icetip: - dump .toc
    git-svn-id: svn://svn.curseforge.net/wow/icetip/mainline/trunk@72 007ea5aa-0d4a-491f-903c-63b4d4e33813

--- modules/buff.lua
+++ modules/buff.lua
@@ -21,31 +21,31 @@ function mod:CreateAuraFrame()
 end
 
 function mod:OnTooltipShow()
-	if buffFrame then return end
-	buffFrame = CreateFrame("Frame", nil, GameTooltip);
-	--buff frame grow dir
-	buffFrame.growBy = "up";
-	buffFrame:SetBackdrop({
-		bgFile = [[Interface/Tooltips/UI-Tooltip-Background]],
-		edgetFile = [[Interface/Tooltips/UI-Tooltip-Border]],
-		tile = false,
-		tileSize = 8,
-		edgetSize = 16,
-		insets = {
-			left = 5,
-			right = 5,
-			top = 5,
-			bottom = 5
-		}
-	});
-	buffFrame:SetBackdropBorderColor(0, 0, 0, 0.6);
-	buffFrame:SetBackdropColor(0, 0, 0, 0.5);
-	
+	--if buffFrame then return end
+	--buffFrame = CreateFrame("Frame", nil, GameTooltip);
+	----buff frame grow dir
+	--buffFrame.growBy = "up";
+	--buffFrame:SetBackdrop({
+	--	bgFile = [[Interface/Tooltips/UI-Tooltip-Background]],
+	--	edgetFile = [[Interface/Tooltips/UI-Tooltip-Border]],
+	--	tile = false,
+	--	tileSize = 8,
+	--	edgetSize = 16,
+	--	insets = {
+	--		left = 5,
+	--		right = 5,
+	--		top = 5,
+	--		bottom = 5
+	--	}
+	--});
+	--buffFrame:SetBackdropBorderColor(0, 0, 0, 0.6);
+	--buffFrame:SetBackdropColor(0, 0, 0, 0.5);
+	--
 
-	--position
-	buffFrame:SetPoint("BOTTOMLEFT", GameTooltip, "TOPLEFT", 2, 0);
-	buffFrame:SetPoint("BOTTOMRIGHT", GameTooltip, "TOPRIGHT", 2, 0);
-	buffFrame:SetHeight(100)
+	----position
+	--buffFrame:SetPoint("BOTTOMLEFT", GameTooltip, "TOPLEFT", 2, 0);
+	--buffFrame:SetPoint("BOTTOMRIGHT", GameTooltip, "TOPRIGHT", 2, 0);
+	--buffFrame:SetHeight(100)
 end
 
 function mod:OnTooltipHide()
